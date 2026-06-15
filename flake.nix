adc02f9ffc2bc4920d11bfbf5af289b672c048d4
montoshitaa <kristel.montoya.chaves@est.una.ac.cr>
2026-06-14 20:22:56 -0600

    feat(02-03): add tickets dependency group and qrcode/pillow to nix flake

--- flake.nix
+++ flake.nix
@@ -30,6 +30,8 @@
             python314Packages.flake8
             python314Packages.textual
             python314Packages.pyside6
+            python314Packages.qrcode
+            python314Packages.pillow
             uv
 
             nodejs
